4ec60ff8835d784a5d7936ae6e11d7f4a04de5c6
taitep <taitep@taitep.se>
2024-12-28 14:36:05 +0100

    Continure working on the emulator

--- sample_implementations/ember_emulator/src/errors.rs
+++ sample_implementations/ember_emulator/src/errors.rs
@@ -5,3 +5,12 @@ use thiserror::Error;
 pub struct InvalidRegister {
     pub index: usize,
 }
+
+#[derive(Debug, Error, Clone, Copy)]
+#[error("invalid flag index {index} (flag indices go from 0-4 exclusive)")]
+pub struct InvalidFlag {
+    pub index: usize,
+}
+
+#[derive(Debug, Error, Clone, Copy)]
+pub enum EmberExecutionError {}
--- sample_implementations/ember_emulator/src/lib.rs
+++ sample_implementations/ember_emulator/src/lib.rs
@@ -1,9 +1,12 @@
+mod alu;
 mod errors;
 
 #[derive(Debug)]
 pub struct Ember {
     ram: [u8; u16::MAX as usize + 1],
     regs: [Register; 4],
+    flags: Flags,
+    exec_state: ExecutionState,
 }
 
 /// Main CPU state struct. Contains all current machine state including RAM.
@@ -17,6 +20,8 @@ impl Ember {
                 Register::ValueReg(0),
                 Register::ValueReg(0),
             ],
+            flags: Flags::default(),
+            exec_state: ExecutionState::Running,
         }
     }
 
@@ -49,11 +54,23 @@ impl Ember {
         self.ram[address as usize] = value as u8;
         self.ram[address.wrapping_add(1) as usize] = (value >> 8) as u8;
     }
+
+    pub fn exec_state(&self) -> ExecutionState {
+        self.exec_state
+    }
+
+    pub fn get_flags(&self) -> Flags {
+        self.flags
+    }
+
+    pub fn get_flags_mut(&mut self) -> &mut Flags {
+        &mut self.flags
+    }
 }
 
 /// Represents a single register and contains its data (or marks it as a zero register)
 #[derive(Debug, Clone, Copy)]
-pub enum Register {
+enum Register {
     ZeroReg,
     ValueReg(u16),
 }
@@ -69,6 +86,97 @@ impl Register {
     pub fn set(&mut self, value: u16) {
         if let Register::ValueReg(x) = self {
             *x = value;
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum ExecutionState {
+    Running,
+    Halted {
+        error: Option<errors::EmberExecutionError>,
+    },
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Flags {
+    pub flag_carry: bool,
+    pub flag_zero: bool,
+    pub flag_sign: bool,
+    pub flag8_carry: bool,
+    pub flag8_zero: bool,
+    pub flag8_sign: bool,
+}
+
+impl Default for Flags {
+    fn default() -> Self {
+        Self {
+            flag_carry: false,
+            flag_zero: false,
+            flag_sign: false,
+            flag8_carry: false,
+            flag8_zero: false,
+            flag8_sign: false,
+        }
+    }
+}
+
+impl Flags {
+    pub fn get(&self, index: usize, is_8bit: bool) -> Option<bool> {
+        match index {
+            0 => Some(true),
+            1 => Some(if is_8bit {
+                self.flag8_carry
+            } else {
+                self.flag_carry
+            }),
+            2 => Some(if is_8bit {
+                self.flag8_zero
+            } else {
+                self.flag_zero
+            }),
+            3 => Some(if is_8bit {
+                self.flag8_sign
+            } else {
+                self.flag_sign
+            }),
+            _ => None,
+        }
+    }
+
+    pub fn set(
+        &mut self,
+        index: usize,
+        value: bool,
+        is_8bit: bool,
+    ) -> Result<(), errors::InvalidFlag> {
+        match index {
+            0 => Ok(()),
+            1 => {
+                if is_8bit {
+                    self.flag8_carry = value;
+                } else {
+                    self.flag_carry = value;
+                }
+                Ok(())
+            }
+            2 => {
+                if is_8bit {
+                    self.flag8_zero = value;
+                } else {
+                    self.flag_zero = value;
+                }
+                Ok(())
+            }
+            3 => {
+                if is_8bit {
+                    self.flag8_sign = value;
+                } else {
+                    self.flag_sign = value;
+                }
+                Ok(())
+            }
+            _ => Err(errors::InvalidFlag { index }),
         }
     }
 }
